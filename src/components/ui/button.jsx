ea9f2e8eca650f57d4b7c44dd67f3658c632f0e0
natalybarah <nathalybs2021@outlook.es>
2026-01-04 11:53:45 -0800

    finalized discover page and responsiveness

---
+++ src/components/ui/button.jsx
@@ -0,0 +1,13 @@
+
+
+const ActionButton= ()=>{
+
+    return(
+        <div className="bg-brand-solid flex justify-center shadow-2xs/20 w-fit px-3.5 py-0.5 rounded-xl">
+            <button className="font-semibold text-sm text-text-brand-emphasis">Explore all</button>
+        </div>
+    )
+}
+
+export default ActionButton;
+// min-w-[87px]
