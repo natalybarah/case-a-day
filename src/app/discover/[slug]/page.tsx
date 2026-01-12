9b3afd500f6542f2d8c62de9db8d57b2ea836030
natalybarah <nathalybs2021@outlook.es>
2026-01-12 05:45:53 -0800

    finalized collection page

--- src/app/discover/[slug]/page.tsx
+++ src/app/discover/[slug]/page.tsx
@@ -13,24 +13,28 @@ const Collection= ()=>{
     const handleScroll=(e: React.UIEvent<HTMLDivElement>)=>{
       const scrollPosition=  e.currentTarget.scrollTop
 
-      if(scrollPosition > 250){
+      if(scrollPosition > 10){
         setIsScrolled(true)
       } else {
         setIsScrolled(false)
-      }
+      } 
 
     }
+    //backdrop-blur-sm
     return(
-        <div onScroll={handleScroll} className="bg-library h-full flex flex-col p-4  gap-5 overflow-y-scroll xs:overflow ">
-            <div className={`fixed top-0 left-1/2 -translate-x-1/2 w-full md:max-w-[430px] z-[60]  flex items-center p-4 transition-all duration-300 
+        <div onScroll={handleScroll} className="bg-library h-full flex flex-col p-4 pt-12 gap-5 overflow-y-scroll xs:overflow ">
+            <div className={`fixed top-0 left-1/2 -translate-x-1/2 w-full md:max-w-[430px] z-60 flex items-center p-4 gap-3 transition-all duration-300 
                 ${isScrolled ? "backdrop-blur-sm border-b border-white/10 shadow-md" : "bg-transparent"} `}>
                 <ArrowLeft color="#C7CDD4" />
-                <p className="font-semibold text-[18px] text-text-brand-emphasis font-fraunces">Wait, like it's hard?</p>
+               { isScrolled ?  <p className="font-semibold  text-text-brand-emphasis font-fraunces 
+                transition-all duration-700 text-[16px]">Wait, like it's hard?</p> : null}
+                
             </div>
             <div className=" relative">
                 <Image src={CollectionImage} alt="woman lawyer in court"  height={350} className="z-30 rounded-[20px] w-full  aspect-16/7 object-cover object-[60%_35%] relative" />
                 <div className="flex flex-col gap-2 absolute inset-0 z-50 justify-end p-4">
-                    <p className="text-text-brand-emphasis font-fraunces font-bold text-[22px]">WAIT, LIKE IT'S HARD</p>  
+                
+                    <p className=" text-text-brand-emphasis font-fraunces font-bold text-[22px]">WAIT, LIKE IT'S HARD</p> 
                     <Chip message="Collection"/>
                 </div>
             </div>
@@ -42,10 +46,7 @@ const Collection= ()=>{
                 <CaseCard/>
                 <CaseCard/>
                 <CaseCard/>
-                <CaseCard/>
-                <CaseCard/>
-                <CaseCard/>
-                <CaseCard/>
+                
             </div>
                     
         </div>
