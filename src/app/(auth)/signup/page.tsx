7409af5e067319ecec6ba4a11e326efead8b409b
natalybarah <nathalybs2021@outlook.es>
2026-01-14 05:50:53 -0800

    finalized styles for signup page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,38 @@
 
+import Link from "next/link";
+import UserInput from "@/src/components/ui/user-input";
+import ActionButton from "@/src/components/ui/button";
+import { ArrowLeft, Mail, LockKeyhole, User } from "lucide-react";
 
 const SignUp= ()=>{
 
     return(
-        <div>
-            <h1>Register!</h1>
-        </div>
+            <div className="flex flex-col bg-neutral-tertiary h-dvh p-4  items-center justify-center gap-8 relative ">
+                    <Link href="/signin" className="self-start top-6 absolute">
+                        <ArrowLeft color="#C7CDD4"/>
+                    </Link>
+                    <div className="flex flex-col p-4  gap-8 justify-center items-center">
+                        <h1 className=" font-bold text-[30px] text-text-on-brand text-center">CREATE ACCOUNT</h1>
+                        <p className="font-semibold text-text-neutral-secondary text-center">Start saving your cases and sync your bookmarks</p>
+                        <form className="flex flex-col gap-3 w-full">
+                            <UserInput placeholder="Full name" name="name" inputType="text" icon={User}/>
+                            <UserInput placeholder="Email" name="email" inputType="text" icon={Mail} />
+                            <UserInput placeholder="Password" name="password" inputType="password" icon={LockKeyhole}/>
+                            <UserInput placeholder="Confirm password" name="confirmPassword" inputType="password" icon={LockKeyhole}/>
+                            <ActionButton impact="boldLarge" message="Create an account"/>
+                        </form>
+                        <div className="text-center">
+                            <p className="text-text-neutral-secondary font-medium text-[16px]">{"Already have an account?"}</p>
+                            <Link className="text-text-brand-accent font-semibold text-[16px]" href="/signin">Sign in</Link>
+                        </div>
+                       {/* <div className="flex flex-row gap-10">
+                            <button><Image src={FacebookIcon} width={44} height={44} alt="facebook icon"/></button> 
+                            <button><Image src={GoogleIcon} width={44} height={44} alt="google icon"/></button> 
+                            <button><Image src={LinkedinIcon} width={44} height={44} alt="linkedin icon"/></button> 
+                        </div>*/}
+                          
+                    </div>
+                </div>
     )
 }
 
--- src/app/(main)/discover/[slug]/page.tsx
+++ src/app/(main)/discover/[slug]/page.tsx
@@ -5,7 +5,7 @@ import CollectionImage from '../../../assets/waitlikeitshard.jpg';
 import Chip from '@/src/components/ui/chip';
 import CaseCard from '@/src/components/case-card';
 import { useState } from 'react';
-//flex-row gap-2 pb-4 border-b border-white/8 backdrop-blur-lg old classes for header
+
 const Collection= ()=>{
 
     const [isScrolled, setIsScrolled] = useState(false)
@@ -18,9 +18,8 @@ const Collection= ()=>{
       } else {
         setIsScrolled(false)
       } 
-
     }
-    //backdrop-blur-sm
+
     return(
         <div onScroll={handleScroll} className="bg-library h-full flex flex-col p-4 pt-12 gap-5 overflow-y-scroll xs:overflow ">
             <div className={`fixed top-0 left-1/2 -translate-x-1/2 w-full md:max-w-[430px] z-60 flex items-center p-4 gap-3 transition-all duration-300 
@@ -28,12 +27,10 @@ const Collection= ()=>{
                 <ArrowLeft color="#C7CDD4" />
                { isScrolled ?  <p className="font-semibold  text-text-brand-emphasis font-fraunces 
                 transition-all duration-700 text-[16px]">Wait, like it's hard?</p> : null}
-                
             </div>
             <div className=" relative">
                 <Image src={CollectionImage} alt="woman lawyer in court"  height={350} className="z-30 rounded-[20px] w-full  aspect-16/7 object-cover object-[60%_35%] relative" />
                 <div className="flex flex-col gap-2 absolute inset-0 z-50 justify-end p-4">
-                
                     <p className=" text-text-brand-emphasis font-fraunces font-bold text-[22px]">WAIT, LIKE IT'S HARD</p> 
                     <Chip message="Collection"/>
                 </div>
@@ -45,10 +42,8 @@ const Collection= ()=>{
                 <CaseCard/>
                 <CaseCard/>
                 <CaseCard/>
-                <CaseCard/>
-                
-            </div>
-                    
+                <CaseCard/>    
+            </div>      
         </div>
     )
 }
